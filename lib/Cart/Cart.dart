277c8377c5aa3e2b3dd713cd8dc98210abde6d37
guanxiang9416 <guanxiang@igola.com>
2019-08-12 19:21:41 +0800

    16进制颜色 + Container.decoration

--- lib/Cart/Cart.dart
+++ lib/Cart/Cart.dart
@@ -15,6 +15,73 @@ class _CartState extends State<CartPage>{
       appBar: AppBar(
         title: Text("购物车"),
       ),
+      body: new Container(
+        height: 300,
+        width: 300,
+        decoration: new BoxDecoration(
+          color: Colors.black26,
+          border: Border.all(color: Colors.red, width: 10),
+          borderRadius: new BorderRadius.all(const Radius.elliptical(10, 10))
+        ),
+        child: new Column(
+          children: <Widget>[
+            new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new Container(
+                    height: 30,
+                    decoration: new BoxDecoration(
+                      border:  new Border.all(width: 5, color: Colors.black38),
+                      borderRadius: const BorderRadius.all(const Radius.circular(8))
+                    ),
+                    margin: const EdgeInsets.all(4),
+                    child: new Image.asset("assets/images/hotels_activated.png"),
+                  ),
+                ),
+
+                new Expanded(
+                  child: new Container(
+                    height: 100,
+                    decoration: new BoxDecoration(
+                      border:  new Border.all(width: 5, color: Colors.black38),
+                      borderRadius: const BorderRadius.horizontal(
+                        left: const Radius.circular(8)
+                      )
+                    ),
+                    margin: const EdgeInsets.all(4),
+                    child: new Image.asset("assets/images/hotels_activated.png"),
+                  ),
+                ),
+
+                new Expanded(
+                  child: new Container(
+                    height: 100,
+                    decoration: new BoxDecoration(
+                      border:  new Border.all(width: 5, color: Colors.black38),
+                      borderRadius: const BorderRadius.vertical(
+                        top: const Radius.circular(8)
+                      )
+                    ),
+                    margin: const EdgeInsets.all(4),
+                    child: new Image.asset("assets/images/hotels_activated.png"),
+                  ),
+                ),
+
+                new Expanded(
+                  child: new ClipOval(
+                    child: new Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black38,
+                      child: new Image.asset("assets/images/hotels_activated.png"),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/Tools/ColorTool.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+/// 颜色工具类, 16进制
+class ColorTool{
+
+  /// 颜色创建方法
+  /// - [hex] 颜色值
+  /// - [alpha] 透明度(默认1，0-1)
+  /// 
+  /// 可以输入多种格式的颜色代码，如: 0x000000,0xff000000,#000000
+  static Color hexColor(String hex, {double alpha}){
+
+    String colorStr = hex.toUpperCase();
+
+    // colorString未带0xff前缀并且长度为6 如"FFBBCC"
+    if (!colorStr.startsWith('0XFF') && colorStr.length == 6){
+      colorStr = '0XFF' + colorStr;
+    }
+
+    // colorString为8位，如0x000000
+    if (colorStr.startsWith('0X') && colorStr.length == 8){
+      colorStr = colorStr.replaceRange(0, 2, '0XFF');
+    }
+
+    // colorString为7位，如#000000
+    if (colorStr.startsWith('#') && colorStr.length == 7){
+      colorStr = colorStr.replaceRange(0, 1, '0XFF');
+    }
+
+    Color color = Color(int.parse(colorStr.toLowerCase()));
+    int red = color.red;
+    int green = color.green;
+    int blue = color.blue;
+    return new Color.fromRGBO(red, green, blue, alpha);
+  }
+
+}
